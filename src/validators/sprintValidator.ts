6ae8e48da8151120ce665c254122b25280684570
Alex Sokol <alexander.j.sokol@gmail.com>
2023-10-01 23:53:07 -0500

    Removing console.log

--- src/validators/sprintValidator.ts
+++ src/validators/sprintValidator.ts
@@ -6,7 +6,6 @@ export const validateGetSprints = [
   check('pageSize').optional().isInt(),
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
-    console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
